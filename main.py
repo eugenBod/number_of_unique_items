958c07dbfe408151da721836eabfe5dbdb3df7a5
eugenBod <eugenybod@gmail.com>
2025-04-22 13:52:13 +0300

    Python number of unique items (ver. 1.0)

---
+++ main.py
@@ -0,0 +1,5 @@
+my_list = [1, 1, 2, 3, 4, 4, 5, 6, 7, 7, 7, 8, 9, 9, 0, 0]
+
+unique_elements = set(my_list)
+count_unique = len(unique_elements)
+print(f"Колличество уникальных элементов: {count_unique}")
